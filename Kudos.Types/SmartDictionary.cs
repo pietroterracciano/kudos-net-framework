74674b4962c0a19ad52414fc7a416838631cb3ca
Pietro Terracciano <gecdeveloper1@gmail.com>
2021-06-18 14:02:08 +0200

    SmartDictionary

---
+++ Kudos.Types/SmartDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudos.Types
+{
+    public class SmartDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                TValue value;
+                TryGetValue(key, out value);
+                return value;
+            }
+            set { base[key] = value; }
+        }
+    }
+}
